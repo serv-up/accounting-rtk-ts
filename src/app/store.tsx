d403fe035f33599536119d2b448704eec9e3f343
NexEnd <104933746+serv-up@users.noreply.github.com>
2025-04-15 10:11:41 +0200

    Add store subscribe and localStorage

--- src/app/store.tsx
+++ src/app/store.tsx
@@ -1,12 +1,19 @@
 import {configureStore} from "@reduxjs/toolkit";
 import user from "../features/user/userSlice.ts";
 import token from "../features/token/tokenSlice.ts"
+import {UserProfile} from "../utils/types";
+
+
+const preloadedState = JSON.parse(localStorage.getItem("state") || "{}") as { user: UserProfile, token: string };
 
 export const store = configureStore({
     reducer: {
         user, token
-    }
+    },
+    preloadedState,
 });
+
+store.subscribe(() => localStorage.setItem("state", JSON.stringify(store.getState())));
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
